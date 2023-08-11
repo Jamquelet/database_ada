b6802872261aac7b50698b4b5f8055d501d11dd8
Jose Maya <jamquelet@hotmail.com>
2023-08-10 20:45:49 -0500

    SQLite Lab2

--- SQLite/SQLite_2_Lab.sql
+++ SQLite/SQLite_2_Lab.sql
@@ -18,3 +18,31 @@ ID	Nombre	Edad	Departamento
 10	Isabel	33	Ventas
 Realiza una consulta SELECT para mostrar el nombre y la edad de los empleados que pertenecen al departamento de "Ventas". (en solucion_1.sql)
 Realiza otra consulta SELECT para mostrar el ID, el nombre y el departamento de los empleados cuya edad sea mayor a 30. (en solucion_2.sql) */
+
+CREATE TABLE Empleados(
+	ID INT PRIMARY KEY NOT NULL,
+	Nombre TEXT,
+	Edad INT,
+	Departamento TEXT
+);
+
+INSERT INTO Empleados
+VALUES 
+	(1, 'Juan', 25, 'Ventas'),
+	(2, 'Maria', 32, 'Recursos Humanos'),
+	(3, 'Pedro', 28, 'Marketing'),
+	(4, 'Ana', 30, 'Ventas'),
+	(5, 'Carlos', 35, 'Recursos Humanos'),
+	(6, 'Laura', 27, 'Marketing'),
+	(7, 'Miguel', 29, 'Ventas'),
+	(8, 'Sofia', 31, 'Recursos Humanos'),
+	(9, 'Luis', 26, 'Marketing'),
+	(10, 'Isabel', 3, 'Ventas');
+	
+SELECT Nombre, Edad
+FROM Empleados
+WHERE Departamento = 'Ventas'
+
+SELECT ID, Nombre, Departamento
+FROM Empleados
+WHERE Edad > 30
