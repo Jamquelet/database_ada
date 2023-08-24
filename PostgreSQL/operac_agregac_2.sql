0b0268930a38d66858eb6f3e9ba5c206322342f0
Jose Maya <jamquelet@hotmail.com>
2023-08-23 19:21:19 -0500

    agregaciones sobre grupos postgresql

--- PostgreSQL/operac_agregac_2.sql
+++ PostgreSQL/operac_agregac_2.sql
@@ -53,3 +53,42 @@ Por ejemplo, supongamos que tenemos una tabla llamada "Ventas" con las columnas 
 SELECT SUM(Cantidad) AS TotalVentas FROM Ventas;
 
 --Esto devolvería un único valor que representa la suma de todas las cantidades de venta en la tabla "Ventas". En este caso, la columna resultante se llamaría "TotalVentas".
+
+----------------------------------------------------------------
+
+--Agregaciones sobre grupos
+/* Cuando necesitamos agrupar filas en categorías específicas y calcular agregaciones sobre cada grupo, se utiliza la instrucción GROUP BY. Esto nos permite realizar cálculos y resúmenes de datos más específicos y segmentados.
+
+Supongamos que tenemos una tabla llamada "Ventas" con las columnas "Cliente_ID", "Producto", "Fecha" y "Cantidad" que registra las ventas de diferentes productos en diferentes fechas.
+
+Si queremos calcular el total de ventas por producto, podemos utilizar la instrucción GROUP BY junto con la función de agregación SUM de la siguiente manera: */
+
+SELECT producto, SUM(cantidad) AS total_ventas
+FROM ventas
+GROUP BY producto; --Esto agrupará las filas de la tabla "Ventas" por cada valor único en la columna "Producto" y calculará la suma de la cantidad para cada grupo.
+
+--Si deseamos obtener el total de ventas por producto y fecha, podemos utilizar el GROUP BY con dos columnas de la siguiente manera
+SELECT producto, fecha, SUM(cantidad) AS TotalVentas
+FROM ventas
+GROUP BY producto, fecha;
+--Esto agrupará las filas por cada combinación única de valores en las columnas "Producto" y "Fecha" y calculará la suma de la cantidad para cada grupo.
+--Al utilizar el GROUP BY, es importante tener en cuenta que solo se pueden incluir en la cláusula SELECT las columnas utilizadas en el GROUP BY o funciones de agregación aplicadas a otras columnas. Esto se debe a que el GROUP BY define las categorías de agrupación y, por lo tanto, las columnas en el resultado deben ser coherentes con las columnas utilizadas para agrupar.
+
+--Filtrando luego de una agregación
+/* Si intentamos filtrar luego de agrupar con GROUP BY, tenemos que utilizar la clausula HAVING.
+
+La cláusula WHERE se utiliza para aplicar condiciones de filtrado a las filas individuales de una tabla antes de que se realice la agrupación o agregación. Por otro lado, la cláusula HAVING se utiliza para aplicar condiciones de filtrado a los grupos formados por la cláusula GROUP BY después de que se haya realizado la agregación.
+
+La sintaxis de la instrucción HAVING es similar a la cláusula WHERE y se utiliza de la siguiente manera: */
+SELECT columnas
+FROM tabla
+GROUP BY columnas
+HAVING condiciones;--que deben cumplir los grupos para ser incluidos en el resultado
+--HAVING se aplica después de la agrupación, esto significa que se pueden utilizar funciones de agregación como COUNT, SUM o AVG en las condiciones de filtrado
+
+--Supongamos que tenemos una tabla llamada "Ventas" con las columnas "Producto" y "Cantidad" que registra las ventas diarias de diferentes productos. Si queremos obtener los productos cuya cantidad total vendida es mayor que 100, podemos utilizar la cláusula HAVING de la siguiente manera:
+
+SELECT producto, SUM(cantidad) AS total_ventas
+FROM ventas
+GROUP BY producto
+HAVING SUM(cantidad) > 100;
