c2139d7a3492dad52e6b0e90c734e084810bcc09
Jose Maya <jamquelet@hotmail.com>
2023-09-13 19:27:13 -0500

    SQLAlquemy - Tortoise ORM

--- ORMs/introducion.sql
+++ ORMs/introducion.sql
@@ -14,3 +14,39 @@ Mantenibilidad: Al utilizar un ORM, los cambios en la estructura de la base de d
 Portabilidad: Los ORM suelen ser independientes de la base de datos, lo que facilita la portabilidad de la aplicación a diferentes entornos. */
 
 --Django ORM para Python, Ruby on Rails para Ruby, SQLAlchemy para Python, entre otros. Cada ORM tiene sus propias características y ventajas, por lo que es importante seleccionar el que mejor se adapte a los requisitos y preferencias del proyecto.
+
+----------------------------------------------------------------
+
+/* a. ¿Qué es un ORM? : 
+Una herramienta para la manipulación de bases de datos.
+
+b. En SQLAlchemy, ¿cuál de las siguientes opciones representa una consulta utilizando SQL puro?: 
+session.execute("SELECT * FROM users WHERE name = 'Jhon'").fetchone()
+
+c. ¿Qué función de Tortoise ORM se utiliza para realizar una agregación como el promedio de un campo?: 
+AVG 
+
+d. En Tortoise ORM, ¿cómo se realiza una consulta que involucre una relación entre tablas utilizando joins?: 
+await TABLE.filter(field_related_field_value = value)
+
+e. En Tortoise ORM, ¿cuál es la forma correcta de realizar una consulta para obtener todas las ventas realizadas en una fecha específica?:
+await Venta.filter(fecha_hora_date = date)
+ */
+
+
+----------------------------------------------------------------
+
+/* Parte 7: CRUD con ORM
+Para realizar la siguiente tarea primero necesitarás crear el mapeo de las tablas a Tortoise
+
+La tarea consiste en crear un CRUD por terminal
+
+Ejecutar el programa en un while infinito
+Listar las tablas con un número al lado (Para que el usuario seleccione con cuál operar)
+Listar las operaciones (crear, listar, obtener, eliminar)
+Dada una selección
+Crear: leer los datos por terminal campo por campo y al final insertar a la db
+Listar: mostrar todas las filas de la tabla
+Obtener: filtrar por id
+Eliminar: filtrar por id
+Con esto creas una interfáz básica para poder crear datos mediante terminal, imagina que desplegaras este sistema para que los usuarios vayan registrando la información terminarías con una base de datos similar a la que te dimos aquí, a partir de estos datos es que podemos realizar consultas analíticas!. En el siguiente curso verás cómo generar reportes a partir de datos!. */
