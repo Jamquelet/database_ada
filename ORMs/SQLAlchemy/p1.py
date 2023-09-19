fcce17605f98555012c9ff573f1385667129ba5c
Jose Maya <jamquelet@hotmail.com>
2023-09-19 16:52:12 -0500

    SQLAlchemy

--- ORMs/SQLAlchemy/p1.py
+++ ORMs/SQLAlchemy/p1.py
@@ -1,3 +1,7 @@
+'''instalar: docs.sqlalchemy.org = pip install SQLAlchemy o conda install sqlalchemy
+
+En un notebook con ! se conecta directamente con la termina !pip install SQLAlchemy ''' 
+
 """ Supongamos que tenemos una base de datos con una tabla llamada "Usuarios" que contiene información sobre los usuarios registrados en nuestra aplicación, incluyendo los campos "id", "nombre" e "email". """
 
 from sqlalchemy import create_engine
@@ -5,11 +9,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String
 
 # Configurar la conexión a la base de datos
-engine = create_engine('sqlite:///usuarios.db')
-Session = sessionmaker(bind=engine)
-session = Session()
+engine = create_engine('sqlite:///usuarios.sqlite', echo=True) # crear una conexion con la base de datos con un archivo usuarios.sqlite para que funcione la extension en vsc.#echo para mostrar el sql que hay detras
+Session = sessionmaker(bind=engine) #creo una session con la que me conecto a la base de datos
+session = Session() # inicializo la session
+
+#documentation: engine configuration engine = create_engine('postgresql+psycopg2://localhost:)
+#url de conexion postgresql desde el dbeaver: edit conection - Server - URL sin el jdbc:
 
 # Definir la clase Usuario
+#crear tabla
 Base = declarative_base()
 
 class Usuario(Base):
@@ -18,9 +26,28 @@ class Usuario(Base):
     nombre = Column(String)
     email = Column(String)
 
-# Consulta utilizando SQLAlchemy
-usuarios = session.query(Usuario).all()
+    def __repr__(self) -> str:
+        return f"Usuario(id={self.id}, nombre={self.nombre}, email={self.email})"
 
+#crea las tablas 
+Base.metadata.create_all(engine)
+
+
+#como creo una instancia de la tabla usuarios- cada instancia del objeto es una fila de la tabla
+u1 = Usuario(nombre='Dad Lovelace', email='dadlovelace@gmail.com',)
+print(u1)
+session.add(u1) #interactuar con la db, agregar esa fila  y la mandamos a la base de datos
+session.commit()
+
+u2 = Usuario(nombre='Julio', email='jjuuull@gmail.com',)
+print(u2)
+session.add(u2) #interactuar con la db, agregar esa fila  y la mandamos a la base de datos
+session.commit()
+
+# Consulta utilizando SQLAlchemy.sacar los datos
+usuarios = session.query(Usuario).all()#dame todos los usuarios
+
+#iterar sobre estos datos
 for usuario in usuarios:
     print(usuario.nombre, usuario.email)
 
@@ -28,6 +55,8 @@ for usuario in usuarios:
 """  definimos la clase Usuario utilizando SQLAlchemy, donde cada columna de la tabla se representa como un atributo de clase. Luego, utilizamos session.query(Usuario).all() para realizar una consulta que obtiene todos los registros de la tabla y almacenamos los resultados en la variable usuarios. Finalmente, recorremos los objetos Usuario obtenidos e imprimimos su nombre y correo electrónico.
 
 La línea session.query(Usuario).all() genera un SQL equivalente a SELECT * FROM Usuarios, de hecho podemos ejecutar consultas SQL directamente desde python de la siguiente forma """
+
+#interfaz de bajo nivel de sqlite para python, ejecuta consultas, queries puros, manda desde pythonel query
 
 import sqlite3
 
